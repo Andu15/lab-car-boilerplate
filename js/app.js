460e66e34e6db0ac5351d10d51bbfac17f2023d2
Andu15 <estefania_8_3@hotmail.com>
2022-06-22 23:07:37 -0500

    add nav mobile styles

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 // La funcionalidad de tu proyecto
+const btnMenu = document.querySelector('#btn-menu');
+const headerNav = document.querySelector('#header-nav');
+
+function toggleElement(element, classes) {
+  element.classList.toggle(classes);
+}
+
+btnMenu.addEventListener('click', () => {
+  toggleElement(headerNav, 'none');
+});
