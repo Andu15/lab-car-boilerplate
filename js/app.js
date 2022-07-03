36b355138d699dd5cf0da43d4993485ec25fc241
Andu15 <estefania_8_3@hotmail.com>
2022-07-03 01:50:32 -0500

    add form styles

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 // La funcionalidad de tu proyecto
 const btnMenu = document.querySelector('#btn-menu');
 const headerNav = document.querySelector('#header-nav');
+const btnJob = document.querySelector('#btn-job');
+const sectionForm = document.querySelector('#section-form');
+const btnClose = document.querySelector('#btn-close');
+const form = document.querySelector('#form');
+const btnSubmit = document.querySelector('#btn-submit');
 
 function toggleElement(element, classes) {
   element.classList.toggle(classes);
@@ -8,4 +13,20 @@ function toggleElement(element, classes) {
 
 btnMenu.addEventListener('click', () => {
   toggleElement(headerNav, 'none');
+});
+
+btnJob.addEventListener('click', () => {
+  toggleElement(sectionForm, 'none');
+});
+
+btnClose.addEventListener('click', () => {
+  toggleElement(sectionForm, 'none');
+});
+
+btnSubmit.addEventListener('click', () => {
+  toggleElement(sectionForm, 'none');
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 });
